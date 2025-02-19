f3b01890ea32e3eabfcf0204f42fde5b7b9fdca2
yourma-ko <maximbakhturazov@gmail.com>
2025-02-19 15:55:27 +0500

    initial setup

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using BLL.Interfaces;
+using BLL.Services;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] string? category)
+        {
+            var products = await productService.GetAllAsync();
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                products = await productService.GetByCategoryAsync(category);
+            }
+
+            return Ok(products);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddProduct([FromServices] IProductService productService, Product product)
+        {
+            await productService.AddAsync(product);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateProduct([FromServices] IProductService productService, Product product)
+        {
+            await productService.UpdateAsync(product);
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeleteProduct([FromServices] IProductService productService, string id)
+        {
+            await productService.DeleteAsync(id);
+            return Ok();
+        }
+        [HttpPost("many")]
+        public async Task<ActionResult> AddMany([FromServices] IProductService productService, List<Product> products)
+        {
+            foreach(var product in products)
+            {
+                await productService.AddAsync(product);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,54 @@
+using BLL.Interfaces;
+using BLL.Services;
+using DAL.Interfaces;
+using DAL.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавление контроллеров
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Получение строки подключения к MongoDB
+var mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING")
+    ?? builder.Configuration.GetConnectionString("Mongo");
+
+var mongoClient = new MongoClient(mongoConnectionString);
+var dbName = "LaptopStore";
+var mongoDb = mongoClient.GetDatabase(dbName);
+
+// Регистрация MongoDB в DI-контейнере
+builder.Services.AddSingleton<IMongoDatabase>(mongoDb);
+
+// Регистрация репозиториев
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+// Регистрация сервисов
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddScoped<ICartService, CartService>();
+
+var app = builder.Build();
+
+// Настройка middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
